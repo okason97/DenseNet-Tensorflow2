7f9997ef9772f1581be224d05fe1ef4d65dc77b8
ulises-jeremias <ulisescf.24@gmail.com>
2020-03-04 23:28:51 -0300

    Updated structure and config

--- dockerfiles/tf-py3-jupyter.Dockerfile
+++ dockerfiles/tf-py3-jupyter.Dockerfile
@@ -1,28 +1,28 @@
 ARG DOCKER_ENV=cpu
 
 FROM ulisesjeremias/tf-docker:${DOCKER_ENV}-jupyter
+# DOCKER_ENV are specified again because the FROM directive resets ARGs
+# (but their default value is retained if set previously)
+
+ARG DOCKER_ENV
 
 ADD . /develop
-COPY notebooks /tf/notebooks
 
-# Needed for string testing 
+# Needed for string testing
 SHELL ["/bin/bash", "-c"]
 
-RUN apt-get update -q
-RUN apt-get install -y git nano
+RUN apt-get update -q && \
+    apt-get install -y libsm6 libxext6 libxrender-dev && \
+    apt-get install -y git nano graphviz && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-RUN apt-get clean
-RUN rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+# Install models, scripts, ...
+RUN pip install --upgrade pip && \
+    pip3 install -e /develop && \
+    pip3 install -U tensorflow && \
+    pip3 install tensorflow_datasets && \
+    pip3 install seaborn eli5 shap pydot pdpbox sklearn opencv-python IPython && \
+    if [[ "$DOCKER_ENV" = "gpu" ]]; then echo -e "\e[1;31mINSTALLING GPU SUPPORT\e[0;33m"; pip3 install -U tf-nightly-gpu-2.0-preview tb-nightly; fi
 
-RUN python3 --version
-
-RUN git clone --branch=develop https://github.com/midusi/handshape_datasets.git /tf/lib/handshape_datasets
-RUN pip3 install -e /tf/lib/handshape_datasets
-
-RUN pip3 install sklearn
-RUN if [[ DOCKER_ENV == "gpu" ]]; then pip3 install tf-nightly-gpu-2.0-preview; fi
-
-RUN mkdir -p /.handshape_datasets
-RUN chmod -R a+rwx /.handshape_datasets
-
-WORKDIR /tf
+WORKDIR /develop
--- setup.py
+++ setup.py
@@ -1,42 +1,25 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-
     long_description = fh.read()
 
 setuptools.setup(
-
-     name='densenet',  
-
-     version='0.1',
-
-     author="Gaston Gustavo Rios, Ulises Jeremias Cornejo Fandos",
-
-     author_email="okason1997@hotmail.com, ulisescf.24@gmail.com",
-
-     description="A densenet implementation using tensorflow2",
-
-     long_description=long_description,
-
-     long_description_content_type="text/markdown",
-
-     url="https://github.com/okason97/DenseNet-Tensorflow2",
-
-     packages=setuptools.find_packages(),
-     
-     install_requires=[
-        'tensorflow==2.0.0-alpha0',
-        'numpy',
-     ],
-
-     classifiers=[
-
-         "Programming Language :: Python :: 3",
-
-         "License :: OSI Approved :: MIT License",
-
-         "Operating System :: OS Independent",
-
-     ],
-
+    name='densenet',  
+    version='0.1',
+    author="Gaston Gustavo Rios, Ulises Jeremias Cornejo Fandos",
+    author_email="okason1997@hotmail.com, ulisescf.24@gmail.com",
+    description="A densenet implementation using tensorflow2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/okason97/DenseNet-Tensorflow2",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    'tensorflow==2.0.0-alpha0',
+    'numpy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
